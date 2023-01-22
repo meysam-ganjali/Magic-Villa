31e42596f9504d10ae550f06232d2cb1f33beb0e
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2023-01-22 19:16:40 +0330

    Add Villa Service In MagicVilla_Web

---
+++ MagicVilla_Web/Services/IServices/IVillaService.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_Web.Models.DTOs;
+
+namespace MagicVilla_Web.Services.IServices;
+
+public interface IVillaService:IBaseService
+{
+    Task<T> GetAllAsync<T>();
+    Task<T> GetAsync<T>(int id);
+    Task<T> CreateAsync<T>(VillaCreateDTO dto);
+    Task<T> UpdateAsync<T>(VillaUpdateDTO dto);
+    Task<T> DeleteAsync<T>(int id);
+}
