21367a8a3945abb4f517dc16b6fca781b96147cb
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2023-01-22 19:35:59 +0330

    Create IndexVilla In Controller EndPoint Web

---
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.DTOs;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers {
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private IMapper _mapper;
+
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDto> list = new();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -1,10 +1,18 @@
 using MagicVilla_Web;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
